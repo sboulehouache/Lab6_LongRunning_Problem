56279f14f50ea4d27b9c7abe4436addc19464b59
Boulehouache <sboulehouache@yahoo.com>
2020-10-06 01:00:25 +0100

    Lab6_LongRunning_Problem

--- app/src/main/java/com/example/boulehouache/mythreadapplication/MainActivity.java
+++ app/src/main/java/com/example/boulehouache/mythreadapplication/MainActivity.java
@@ -10,43 +10,45 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String TAG="Service";
-    Button btnBoucle;
+    Button btnLoop;
     Button btnMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnBoucle = (Button) findViewById(R.id.btnBoucle);
-        assert btnBoucle != null;
-        btnBoucle.setOnClickListener(new View.OnClickListener() {
+        btnLoop = (Button) findViewById(R.id.btnBoucle);
+        assert btnLoop != null;
+        btnMessage = (Button) findViewById(R.id.btnMessage);
+        assert btnMessage != null;
+
+
+        btnLoop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnBoucle.setBackgroundColor(Color.BLUE);
+                btnLoop.setBackgroundColor(Color.GREEN);
                 Log.i(TAG, "onClick: Stop Service");
                     try{
                         Thread.sleep(10000);
                     }
                     catch(Exception e){
-
                     }
-                btnBoucle.setBackgroundColor(Color.GREEN);
+                btnLoop.setBackgroundColor(Color.GRAY);
             }
         });
-        btnMessage = (Button) findViewById(R.id.btnMessage);
-        assert btnMessage != null;
+
         btnMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnMessage.setBackgroundColor(Color.BLUE);
                 Log.i(TAG, "onClick: message");
                 Toast toast = Toast.makeText(MainActivity.this, "I am a message", Toast.LENGTH_LONG);
                 toast.show();
-                btnMessage.setBackgroundColor(Color.GREEN);
             }
         });
     }
     @Override
     protected void onResume() {
         super.onResume();
+        try{Thread.sleep(10000);}
+        catch(Exception e){     }
     }
 }
